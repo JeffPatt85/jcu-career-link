8d956b93b8825190c98d988114d520f38120e8b5
TomStanley95 <tom.stanley@my.jcu.edu.au>
2019-09-18 14:22:19 +1000

    Button instead of link

---
+++ public/scripts/onClickManager.js
@@ -0,0 +1,6 @@
+console.log('onClickManager is running');
+
+const button = document.getElementById('notificationButton');
+button.addEventListener('click', function(e) {
+    console.log('button was clicked');
+});
